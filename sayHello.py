1b30ce37f0b5dc46fdba8d05418a85aa2e6ba78c
caesar0016 <94184309+caesar0016@users.noreply.github.com>
2022-12-01 07:31:33 +0800

    verify age

--- sayHello.py
+++ sayHello.py
@@ -1,1 +1,12 @@
 print("Hello, World")
+print(".............")
+age = int(input("Enter Age: "))
+
+print("Your age is " + str(age))
+
+if age > 18:
+    print("................")
+    print("You're legal")
+elif age<18:
+    print("................")
+    print("Minor Momints")
